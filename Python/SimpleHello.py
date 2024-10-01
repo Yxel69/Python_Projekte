baa62d17c386d7e9bea39222712642accca5c02c
Shroomy <80010644+Yxel69@users.noreply.github.com>
2024-10-01 16:55:12 +0200

    retirement of old junk projects
    gargabe truck

--- Python/SimpleHello.py
+++
@@ -1,9 +0,0 @@
-#Eingabe
-name = input("What is your name? : ")   #Speichern von Terminalinput in einer Variable
-#Ausgabe
-print("------------->")                  #Verschönerung der Ausgabe
-print("")                            #Verschönerung der Ausgabe
-print("Hello "+ name)                #Ausgabe der Variable welche den Userinput enthält
-print("")                            #Verschönerung der Ausgabe
-print("------------->")                  #Verschönerung der Ausgabe
-             
